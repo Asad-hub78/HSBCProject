ebf9a631993e2f8b18838317cde3e157fb391d9d
Folder <marchal_001@yahoo.com>
2024-06-24 03:29:59 -1100

    First commit

--- cypress/e2e/Credit_Card_Test.spec.cy.js
+++ cypress/e2e/Credit_Card_Test.spec.cy.js
@@ -1,14 +1,23 @@
-describe('Credit Card', () => {
-  it('Credit Card Test', () => {
-    cy.visit('https://www.hsbc.co.in/');
-    cy.wait(500);
-    cy.get('div.header-mobile-doormat-0.header-doormat-mobile-title.sidebar-submenu-trigger>span:nth-of-type(2)').click({force: true,multiple: true});
-  cy.wait(1000);
-cy.get('a.doormat-heading-link h2.doormat-heading').click({multiple:true,force:true});
-
-cy.get('h1').should('be.visible');
-cy.wait(1000);
-cy.scrollTo(0, 700);
-cy.get('h3#chp_main_link_2>a').click({force:true});
-})
-})
+describe('HSBC India Navigation', () => {
+  it('should hover over the Banking menu', () => {
+    
+   cy.visit('https://www.hsbc.co.in/');       
+   cy.wait(2000);    
+   cy.get('.header-main-navigation-title').contains('Banking').trigger('mouseover');
+   cy.get('a').contains('Credit Cards').click({force: true});
+   cy.url().should('include', '/credit-cards');
+   cy.get('h1').contains('Credit cards').should('be.visible');
+   cy.get('h3').contains('HSBC Cashback Credit Card').should('be.visible');
+   cy.get('a').contains('HSBC Cashback Credit Card').click({force: true});
+   cy.get('a#pp_intro_button_1').contains('Apply now').should('be.visible');   
+   cy.url().should('include', 'credit-cards/products/visa-cashback');    
+   cy.get('#pp_intro_image_3').should('be.visible');
+   cy.get('#pp_tools_richtext_2').contains('INR999').should('be.visible');
+   cy.get('#pp_tools_richtext_3').contains('INR999 (waived if you spend more than INR200,000 per year) ').should('be.visible');
+   cy.get('img[src="https://www.hsbc.co.in/content/dam/hsbc/in/images/webchat/chat-button-en-pwsimg-2841.png"][alt="Chat"]').should('be.visible');   
+   cy.get('[alt="HSBC India Bank"]').first().click();
+   cy.title().should('eq', 'HSBC India - Credit Cards, NRI Services, Saving and Deposit');      
+   cy.screenshot('creditcard');
+           
+        });
+});
--- cypress/e2e/Login_Scnerio.spec.cy.js
+++ cypress/e2e/Login_Scnerio.spec.cy.js
@@ -1,20 +1,23 @@
-describe('Login Scnerio', () => {
-  it('Login Scnerio Test', () => {
-    cy.visit('https://www.hsbc.co.in/');
-    cy.wait(1000);
-    cy.get('h1').should('be.visible');
-    cy.title().should('eq','HSBC India - Credit Cards, NRI Services, Saving and Deposit');
-    cy.wait(1000)
-    cy.get('[aria-label="Log on options"]>a.selected-item.login-button.only-one-link').click({force: true});
+
+describe("HSBC Login Scenario",()=>{
     
-    cy.wait(1000)
-    cy.get('[aria-label="Continue to page"]').click();
-    
-    cy.get('div.col-sm-12.userHeading>h2.pull-left.heading.t28l>span').contains('Log On');
-   
-    cy.get('#username').type('abc@example.com');
-   cy.get('#username_submit_btn').should('be visible');
-   cy.get('.icon.icon-circle-help-solid.help-icon').click();
-    
+  it("Login Test",()=>{
+      cy.visit("https://www.hsbc.co.in/");
+      cy.get("h1").should('be.visible');
+      cy.title().should('eq',"HSBC India - Credit Cards, NRI Services, Saving and Deposit");   
+      cy.get("li[class='header-user-wrapper']>div>a[class='selected-item login-button only-one-link']").click({force: true});
+      cy.get(".crh-chevron-link__text").click();
+      cy.get("div>h2>span").should('be.visible');
+      cy.get("#username_submit_btn").should('be.visible');
+      cy.get("button#username_submit_btn").should('be.disabled');
+      cy.get("input#username").type("hsbc@example.com");
+      cy.get("button#username_submit_btn").should('be.enabled');
+      cy.get("#username_help_link>span>span[aria-hidden='true']").should('be.visible');
+      cy.get("#username_help_link>span>span[aria-hidden='true']").click();
+      cy.get("#help_content_1>h3>span").contains("Username");
+      cy.get("a.close-btn>span>span[aria-hidden='true']").should('be.visible');
+      cy.get("a.close-btn>span>span[aria-hidden='true']").click();
+      cy.screenshot();
+
   })
 })
